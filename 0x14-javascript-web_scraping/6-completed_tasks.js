4d706156dbc5a84934ebea749c6ea0b8d4975d6b
Shadow P <117807310+ShadowP2066@users.noreply.github.com>
2023-07-26 10:55:31 +0200

    mandatory

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,26 @@
 #!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const dict = JSON.parse(body).reduce((acc, elem) => {
-    if (!acc[elem.userId]) {
-      if (elem.completed) {
-        acc[elem.userId] = 1;
-      }
-    } else {
-      if (elem.completed) {
-        acc[elem.userId] += 1;
+const dictList = {};
+
+request(url, { json: true }, (err, res) => {
+  const list = res.body;
+  if (err) {
+    console.log(err);
+  } else {
+    for (let i = 0; i < list.length; i++) {
+      const key = list[i].userId;
+      if (list[i].completed === true) {
+        if (!dictList[key]) {
+          dictList[key] = 1;
+        } else {
+          dictList[key] += 1;
+        }
       }
     }
-    return acc;
-  }, {});
-  console.log(dict);
+    console.log(dictList);
+  }
 });
