c9df13d077936e0b6dde3e4ae90d347aec609992
Shadow P <117807310+ShadowP2066@users.noreply.github.com>
2023-07-26 09:16:28 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+const args = process.argv.slice(2);
+const url = args[0];
+
+const completedTasks = {};
+request.get(url, (err, res, body) => {
+  if (err) throw err;
+  const todos = JSON.parse(body);
+  for (let i = 0; i < todos.length; i++) {
+    let key = todos[i].userId;
+    if (todos[i].completed) {
+      if (!(key in completedTasks)) {
+        completedTasks[key] = 1;
+      } else {
+        completedTasks[key] += 1;
       }
-    });
-    console.log(completed);
+    }
   }
+  console.log(completedTasks);
 });
